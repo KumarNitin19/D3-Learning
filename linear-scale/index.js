da92808c30ed1199b4e95c5c804872112413e7c5
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-05-30 03:32:47 +0530

    feat: dynamic cricle created without scale

--- linear-scale/index.js
+++ linear-scale/index.js
@@ -1,1 +1,37 @@
 console.log("linear scale");
+
+const myData = [
+  {
+    n: 0,
+    p: 0,
+  },
+  {
+    n: 200,
+    p: 200,
+  },
+  {
+    n: 300,
+    p: 300,
+  },
+  {
+    n: 400,
+    p: 400,
+  },
+];
+
+const svgViewPort = d3
+  .select("body")
+  .insert("svg", ":first-child")
+  .attr("width", "200")
+  .attr("height", "200");
+
+const circle = svgViewPort
+  .selectAll("circle")
+  .data(myData)
+  .enter()
+  .append("circle");
+
+const circleAttr = circle
+  .attr("cx", (d) => d.n)
+  .attr("cy", (d) => d.y)
+  .attr("r", "25");
