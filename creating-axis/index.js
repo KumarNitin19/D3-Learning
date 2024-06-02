8ad22f7ac3b1ed1a1a5c61e8bc5647f5e623e9ac
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-02 23:45:58 +0530

    feat: custom axis created using de

--- creating-axis/index.js
+++ creating-axis/index.js
@@ -1,1 +1,13 @@
 console.log("d3 axis");
+
+let myScale = d3.scaleLinear().domain([0, 10]).range([0, 200]);
+
+let myAxis = d3.svg.axis().scale(myScale);
+
+let mySvg = d3
+  .select(".root")
+  .append("svg")
+  .attr("width", "200")
+  .attr("height", "200");
+
+let myAxisGroup = mySvg.append("g").call(myAxis);
