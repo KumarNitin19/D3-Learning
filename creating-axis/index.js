18360d98a61203f9f8bc9385e0e9c3fd2d3bee61
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-04 00:50:31 +0530

    fix: d3 axis created using margin convention

--- creating-axis/index.js
+++ creating-axis/index.js
@@ -19,6 +19,16 @@ const margin = { top: 50, right: 50, bottom: 50, left: 50 };
 const width = 300 - margin.left - margin.right;
 const height = 300 - margin.top - margin.bottom;
 
-const myScale = d3.scaleLinear().domain([0, 15]).range(0, width);
+const myScale = d3.scaleLinear().domain([0, 10]).range([0, width]);
 
-const myAxis = de.axisBotton().scale(myScale);
+const myAxis = d3.axisBottom().scale(myScale);
+
+const svgElement = d3
+  .select(".root")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.bottom}, ${margin.left})`);
+
+const myAxisGroup = svgElement.call(myAxis);
