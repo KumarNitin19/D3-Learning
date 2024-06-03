73a806f4f7c1236dda089719ed87cb6fe17161c6
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-04 01:25:56 +0530

    feat: scatter chart created using axis and coordinates

--- creating-axis/index.js
+++ creating-axis/index.js
@@ -54,3 +54,62 @@ const myYAxisGroupThree = svgElementThree
   .append("g")
   .attr("transform", `translate(${margin.bottom}, ${margin.left})`)
   .call(myYAxisThree);
+
+// Creating scatter graph using d3 axis and scales (scatter_Chart)
+
+const myChartData = [
+  {
+    x: 7,
+    y: 8,
+    r: 7,
+    fill: "red",
+  },
+  {
+    x: 3.5,
+    y: 8.5,
+    r: 3,
+    fill: "blue",
+  },
+  {
+    x: 2,
+    y: 5,
+    r: 5,
+    fill: "yellow",
+  },
+  {
+    x: 9,
+    y: 6,
+    r: 6,
+    fill: "orange",
+    x: 6,
+    y: 2,
+    r: 4,
+    fill: "green",
+  },
+];
+
+const scScale = d3.scaleLinear().domain([0, 10]).range([0, width]);
+
+const scXAxis = d3.axisTop().scale(scScale);
+const scYAxis = d3.axisLeft().scale(scScale);
+
+const scSvgElement = d3
+  .select("#scatter_Chart")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.bottom},${margin.left})`);
+const scXAisGroup = scSvgElement.append("g").call(scXAxis);
+const scYAisGroup = scSvgElement.append("g").call(scYAxis);
+
+const scCircleElement = scSvgElement
+  .append("g")
+  .selectAll("circle")
+  .data(myChartData)
+  .enter()
+  .append("circle")
+  .attr("cx", (d) => scScale(d.x))
+  .attr("cy", (d) => scScale(d.x))
+  .attr("r", (d) => d.r)
+  .attr("fill", (d) => d.fill);
