530b82409fbec64646149adc53b105e4c95ee937
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-04 00:43:20 +0530

    fix: margin conventions applied to d3 axis

--- creating-axis/index.js
+++ creating-axis/index.js
@@ -1,13 +1,24 @@
 console.log("d3 axis");
 
-let myScale = d3.scaleLinear().domain([0, 10]).range([0, 200]);
+// let myScale = d3.scaleLinear().domain([0, 10]).range([0, 200]);
 
-let myAxis = d3.axisBottom().scale(myScale);
+// let myAxis = d3.axisBottom().scale(myScale);
 
-let mySvg = d3
-  .select(".root")
-  .append("svg")
-  .attr("width", "200")
-  .attr("height", "200");
+// let mySvg = d3
+//   .select(".root")
+//   .append("svg")
+//   .attr("width", "200")
+//   .attr("height", "200");
 
-let myAxisGroup = mySvg.append("g").call(myAxis);
+// let myAxisGroup = mySvg.append("g").call(myAxis);
+
+// Using d3 margin convention
+
+const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+
+const width = 300 - margin.left - margin.right;
+const height = 300 - margin.top - margin.bottom;
+
+const myScale = d3.scaleLinear().domain([0, 15]).range(0, width);
+
+const myAxis = de.axisBotton().scale(myScale);
