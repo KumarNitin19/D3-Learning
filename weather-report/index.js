92feaf08f2f8d1b1db09c1a0cb76c07685d8199a
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-05 01:40:48 +0530

    update: data added for top 10 city weather report

---
+++ weather-report/index.js
@@ -0,0 +1,54 @@
+console.log("Weather Report");
+
+const cityData = [
+  {
+    id: 0,
+    area: "Mungeshpur, Delhi",
+    temprature: 53,
+  },
+  {
+    id: 1,
+    area: "Phalodi, Rajasthan",
+    temprature: 51,
+  },
+  {
+    id: 2,
+    area: "Sirsa, Haryana",
+    temprature: 50.3,
+  },
+  {
+    id: 3,
+    area: "Gwalior, Madhya Pradesh",
+    temprature: 48,
+  },
+  {
+    id: 4,
+    area: "Narela, Delhi",
+    temprature: 47.9,
+  },
+  {
+    id: 5,
+    area: "Rohtak, Haryana",
+    temprature: 47.7,
+  },
+  {
+    id: 6,
+    area: "Churu, Rajasthan",
+    temprature: 47.4,
+  },
+  {
+    id: 7,
+    area: "Bikaner, Rajasthan",
+    temprature: 47,
+  },
+  {
+    id: 8,
+    area: "Jagdhishpur, Haryana",
+    temprature: 46.5,
+  },
+  {
+    id: 9,
+    area: "Sri Ganganagar, Rajasthan",
+    temprature: 46,
+  },
+];
