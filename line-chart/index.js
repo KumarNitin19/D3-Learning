66f04f73ed3f7c906eddc18b735182c11513f6e7
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-11 01:18:16 +0530

    fix:dataset added for line chart

--- line-chart/index.js
+++ line-chart/index.js
@@ -1,10 +1,13 @@
 console.log("Line Chart");
 
+const csvFileUrl =
+  "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv";
+
 const margin = { top: 20, right: 20, bottom: 30, left: 50 };
 const height = 500 - margin.top - margin.bottom;
 const width = 960 - margin.left - margin.right;
 
-const parseDate = d3.utcFormat("%d-%b-%y").parse;
+const parseDate = d3.timeParse("%Y-%m-%d");
 
 const x = d3.scaleTime().range([0, width]);
 const y = d3.scaleTime().range([0, height]);
@@ -24,3 +27,14 @@ const svg = d3
   .attr("width", width + margin.left + margin.right)
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv(
+  csvFileUrl,
+  function (d) {
+    return { date: parseDate(d.date), value: d.value };
+  },
+  function (data) {
+    x.domain(d3.extent(data, (d) => d.date));
+    y.domain(d3.extent(data, (d) => d.close));
+  }
+);
