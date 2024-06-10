e958cc68c5ae724942dd368448d072d4b772eccf
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-10 23:58:59 +0530

    fix: update x and y axis added for line chart

--- line-chart/index.js
+++ line-chart/index.js
@@ -3,3 +3,8 @@ console.log("Line Chart");
 const margin = { top: 20, right: 20, bottom: 30, left: 50 };
 const height = 960 - margin.top - margin.bottom;
 const width = 960 - margin.left - margin.right;
+
+const parseDate = d3.time.format("%d-%b-%y").parse;
+
+const x = d3.time.scale().range([0, width]);
+const y = d3.time.scale().range([0, height]);
