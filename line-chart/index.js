9b95f8fbf7f5f3a14a30984ad9a128e4c995c201
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-11 00:07:45 +0530

    update: svg and x, y axis created for line chart

--- line-chart/index.js
+++ line-chart/index.js
@@ -1,10 +1,26 @@
 console.log("Line Chart");
 
 const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-const height = 960 - margin.top - margin.bottom;
+const height = 500 - margin.top - margin.bottom;
 const width = 960 - margin.left - margin.right;
 
-const parseDate = d3.time.format("%d-%b-%y").parse;
+const parseDate = d3.utcFormat("%d-%b-%y").parse;
 
-const x = d3.time.scale().range([0, width]);
-const y = d3.time.scale().range([0, height]);
+const x = d3.scaleTime().range([0, width]);
+const y = d3.scaleTime().range([0, height]);
+
+const xAxis = d3.axisBottom().scale(x);
+const yAxis = d3.axisLeft().scale(y);
+
+const line = d3
+  .line()
+  .x((d) => x(d.date))
+  .y((d) => x(d.close));
+
+const svg = d3
+  .select("#root")
+  .append("svg")
+  .attr("height", height + margin.top + margin.bottom)
+  .attr("width", width + margin.left + margin.right)
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
