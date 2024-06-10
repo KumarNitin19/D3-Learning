00f40a11a517cabb8dcc0a0d3a9095524ad3c035
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-10 23:57:18 +0530

    update: basic margin and height and width setup done for line chart

--- line-chart/index.js
+++ line-chart/index.js
@@ -1,1 +1,5 @@
 console.log("Line Chart");
+
+const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+const height = 960 - margin.top - margin.bottom;
+const width = 960 - margin.left - margin.right;
