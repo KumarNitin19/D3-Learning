e4ae2772058664a2e3314d779fc99ceacba0405d
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-12 01:10:54 +0530

    fix: x and y axis created for scatter plot graph

--- scatter-plot/index.js
+++ scatter-plot/index.js
@@ -1,20 +1,25 @@
 console.log("Scatter Plot Graph");
 
+const scatterPlotDataUrl =
+  "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv";
+
 const margin = {
-  top: 20,
-  right: 20,
-  bottom: 30,
-  left: 40,
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50,
 };
 
 const width = 960 + margin.left - margin.right;
 const height = 500 + margin.top - margin.bottom;
 
-const x = d3.scaleLinear().domain([0, width]);
-const y = d3.scaleLinear().domain([height, 0]);
+const x = d3.scaleLinear().range([0, width]);
+const y = d3.scaleLinear().range([height, 0]);
 
 const xAxis = d3.axisBottom().scale(x);
 const yAxis = d3.axisLeft().scale(y);
+
+const color = d3.scaleOrdinal(d3.schemeCategory10);
 
 const svg = d3
   .select("#root")
@@ -23,3 +28,40 @@ const svg = d3
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", `translate(${margin.left},${margin.top})`);
+
+d3.csv(
+  scatterPlotDataUrl,
+  function (d) {
+    return {
+      grLivArea: +d.GrLivArea,
+      salePrice: +d.SalePrice,
+    };
+  },
+  function (data) {
+    x.domain([0, d3.max(data, (d) => d.grLivArea) + 500]);
+    y.domain([0, d3.max(data, (d) => d.salePrice)]);
+
+    svg
+      .append("g")
+      .attr("class", "x axis")
+      .attr("transform", `translate(0,${height})`)
+      .call(xAxis)
+      .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .attr("text-anchor", "end")
+      .text("GRliveArea");
+
+    svg
+      .append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("class", "label")
+      .attr("transform", `rotate(-90)`)
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .text("Sale Price");
+  }
+);
