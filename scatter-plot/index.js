a0d8d310629b13a9b7015abce9240581e6f10c3a
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-12 00:25:53 +0530

    fix: scales created for scatter plot

--- scatter-plot/index.js
+++ scatter-plot/index.js
@@ -1,1 +1,17 @@
 console.log("Scatter Plot Graph");
+
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 30,
+  left: 40,
+};
+
+const width = 960 + margin.left - margin.right;
+const height = 500 + margin.top - margin.bottom;
+
+const x = d3.scaleLinear().domain([0, width]);
+const y = d3.scaleLinear().domain([height, 0]);
+
+const xAxis = d3.axisBottom().scale(x);
+const yAxis = d3.axisLeft().scale(y);
