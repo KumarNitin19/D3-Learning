cf128b990baed81269c6dff913f9b202b5a7b7f5
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-13 00:41:26 +0530

    update: margin and height, width added for graph

--- bar-chart/index.js
+++ bar-chart/index.js
@@ -1,1 +1,11 @@
 console.log("Bar Chart");
+
+const margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50,
+};
+
+const width = 960 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
