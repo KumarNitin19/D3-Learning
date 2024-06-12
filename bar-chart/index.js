08cdad222d24158fab7c20246f9ed9300892aba4
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-13 00:49:32 +0530

    update: x and y axis added for bar chart

--- bar-chart/index.js
+++ bar-chart/index.js
@@ -9,3 +9,12 @@ const margin = {
 
 const width = 960 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
+
+const formatPercent = d3.format(".0%");
+
+const x = d3.scaleBand().rangeRound([0, width], 0.1);
+
+const y = d3.scaleLinear().range([height, 0]);
+
+const xAxis = d3.axisBottom().scale(x);
+const yAxis = d3.axisLeft().scale(y).tickFormat(formatPercent);
