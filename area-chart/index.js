ae0ec2ce660493260cb49802328de8ab3203870b
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-14 00:45:19 +0530

    feat: x and y axis added for area graph

--- area-chart/index.js
+++ area-chart/index.js
@@ -12,3 +12,9 @@ const margin = {
 
 const width = 960 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
+
+const x = d3.scaleTime().range([0, width]);
+const y = d3.scaleLinear().range([height, 0]);
+
+const xAxis = d3.axisBottom().scale(x);
+const yAxis = d3.axisLeft().scale(y);
