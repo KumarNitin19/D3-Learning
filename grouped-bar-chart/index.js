feb53ec117fe43b817464916ed0683ffd9584807
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-14 01:22:10 +0530

    feat: basic margin and height and width initialize dfor graph

--- grouped-bar-chart/index.js
+++ grouped-bar-chart/index.js
@@ -2,3 +2,13 @@ console.log("Grouped bar chart");
 
 const csvDataUrl =
   "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_stacked.csv";
+
+const margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50,
+};
+
+const width = 960 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
