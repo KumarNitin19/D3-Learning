a91c5b9705e56c9a7fe41a729bc3f6a563c2a6b8
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-14 01:36:43 +0530

    feat: x axis created for graph

--- grouped-bar-chart/index.js
+++ grouped-bar-chart/index.js
@@ -13,11 +13,10 @@ const margin = {
 const width = 960 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
-const x0 = d3.scaleBand().range([0, width]).padding([0.1]);
-const x1 = d3.scaleBand();
+const x = d3.scaleBand().range([0, width]).padding([0.1]);
 const y = d3.scaleLinear().range([height, 0]);
 
-const xAxis = d3.axisBottom().scale(x0);
+const xAxis = d3.axisBottom().scale(x);
 const yAxis = d3.axisLeft().scale(y);
 
 const svg = d3
@@ -27,3 +26,20 @@ const svg = d3
   .attr("width", width + margin.left + margin.right)
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv(csvDataUrl, function (data) {
+  const subgroups = data.columns.slice(1);
+  const groups = d3
+    .map(data, function (d) {
+      return d.group;
+    })
+    .keys();
+
+  x.domain(groups);
+
+  svg
+    .append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxis);
+});
