c47b45fed5854dedf843cd289b4066c249bfd672
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-14 00:42:38 +0530

    feat: height and width added for graph

--- area-chart/index.js
+++ area-chart/index.js
@@ -1,1 +1,14 @@
 console.log("Area Chart");
+
+const csvDataUrl =
+  "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv";
+
+const margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50,
+};
+
+const width = 960 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
