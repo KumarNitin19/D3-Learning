b1e0e3a0db88017178d5d3c7b7b56cbb4ac86a61
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-14 00:55:45 +0530

    feat: csv data loaded and parsed

--- area-chart/index.js
+++ area-chart/index.js
@@ -19,6 +19,8 @@ const y = d3.scaleLinear().range([height, 0]);
 const xAxis = d3.axisBottom().scale(x);
 const yAxis = d3.axisLeft().scale(y);
 
+const parseDate = d3.timeParse("%Y-%m-%d");
+
 const area = d3
   .area()
   .x(function (d) {
@@ -36,3 +38,17 @@ const svg = d3
   .attr("width", width + margin.left + margin.right)
   .append("g")
   .attr("transform", `translate(${margin.left},${margin.top})`);
+
+d3.csv(
+  csvDataUrl,
+  function (d) {
+    return {
+      date: parseDate(d.date),
+      value: d.value,
+    };
+  },
+  function (data) {
+    x.domain(d3.extent(data, (d) => d.date));
+    y.domain([0, d3.max(data, (d) => d.value) + 500]);
+  }
+);
