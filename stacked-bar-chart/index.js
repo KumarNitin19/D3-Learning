80b307cc6ffb2e48308a5da6d2df678523c04565
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-15 00:42:47 +0530

    feat: margins, width and x, y axis added for stacked bar chart

--- stacked-bar-chart/index.js
+++ stacked-bar-chart/index.js
@@ -1,1 +1,20 @@
 console.log("Stacked Bar Chart");
+
+const csvDataUrl =
+  "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_stacked.csv";
+
+const margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50,
+};
+
+const width = 960 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+const x = d3.scaleBand().range([0, width]).padding([0.1]);
+const y = d3.scaleLinear().range([height, 0]);
+
+const xAxis = d3.axisBottom().scale(x);
+const yAxis = d3.axisLeft().scale(y);
