b404af08ac8ee87aaf87576b4be2e127204a8c0b
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-15 00:44:30 +0530

    feat: svg and group element added for stacked bar chart

--- stacked-bar-chart/index.js
+++ stacked-bar-chart/index.js
@@ -18,3 +18,11 @@ const y = d3.scaleLinear().range([height, 0]);
 
 const xAxis = d3.axisBottom().scale(x);
 const yAxis = d3.axisLeft().scale(y);
+
+const svg = d3
+  .select("#root")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
