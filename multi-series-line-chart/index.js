ed91c48c01f227857b36ea26e8c5bde797281dd6
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-06-16 23:51:49 +0530

    fix: height, width and margin added for multi series line chart

--- multi-series-line-chart/index.js
+++ multi-series-line-chart/index.js
@@ -2,3 +2,13 @@ console.log("Multi series line chart");
 
 const csvUrl =
   "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered.csv";
+
+const margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50,
+};
+
+const height = 500 - margin.top - margin.bottom;
+const width = 960 - margin.left - margin.right;
