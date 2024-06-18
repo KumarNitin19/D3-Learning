a4b13fa19cc1b3160b0573c6b193fcf08478d6bd
Nitin kumar <nitinkumardeshukh0123@gmail.com>
2024-06-18 23:57:35 +0530

    feat: svg element added for graph

--- pie-chart/index.js
+++ pie-chart/index.js
@@ -11,3 +11,11 @@ const margin = {
 
 const height = 500 - margin.top - margin.bottom;
 const width = 960 - margin.right - margin.left;
+
+const svg = d3
+  .select("#root")
+  .append("svg")
+  .attr("height", height + margin.top + margin.bottom)
+  .attr("width", width + margin.left + margin.right)
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
