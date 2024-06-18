f1e607e7c1ba6d39d0cc283aa43aeb63717e8b90
Nitin kumar <nitinkumardeshukh0123@gmail.com>
2024-06-18 23:53:11 +0530

    feat: height, width and margin added for graph

--- pie-chart/index.js
+++ pie-chart/index.js
@@ -1,1 +1,13 @@
 console.log("Pie Chart");
+
+const pieChartData = { a: 9, b: 20, c: 30, d: 8, e: 12 };
+
+const margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50,
+};
+
+const height = 500 - margin.top - margin.bottom;
+const width = 960 - margin.right - margin.left;
