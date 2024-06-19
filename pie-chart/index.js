aeb99a16ce63156228550313696a58f4a1869c3b
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-06-20 00:58:30 +0530

    feat: basic pie chart created

--- pie-chart/index.js
+++ pie-chart/index.js
@@ -2,10 +2,16 @@ console.log("Pie Chart");
 
 const pieChartData = { a: 9, b: 20, c: 30, d: 8, e: 12 };
 
-const height = 500;
-const width = 960;
+const height = 450;
+const width = 450;
+const margin = 40;
 
-const radius = d3.min(height, width) / 2;
+const radius = Math.min(height, width) / 2 - margin;
+
+const color = d3
+  .scaleOrdinal()
+  .domain(pieChartData)
+  .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"]);
 
 const svg = d3
   .select("#root")
@@ -14,5 +20,19 @@ const svg = d3
   .attr("width", width)
   .append("g")
   .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+const pie = d3.pie().value((d) => d.value);
+const dataReady = pie(d3.entries(pieChartData));
+
+svg
+  .selectAll(".arc")
+  .data(dataReady)
+  .enter()
+  .append("path")
+  .attr("d", d3.arc().innerRadius(0).outerRadius(radius))
+  .attr("fill", (d) => color(d.data.key))
+  .attr("stroke", "white")
+  .attr("stroke-width", 2)
+  .attr("opcaity", 0.7);
 
 // console.log("test");
