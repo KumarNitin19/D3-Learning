4d70405c04d124b3142e7f522aaf43a70e701b6f
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-06-21 01:37:40 +0530

    feat: svg created for bar chart

--- bar-chart-with-negative-value/index.js
+++ bar-chart-with-negative-value/index.js
@@ -11,3 +11,22 @@ const margin = {
 
 const width = 960 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
+
+const x0 = Math.max(-d3.min(barChartData), d3.max(barChartData));
+
+const x = d3.scaleLinear().domain([-x0, x0]).range([0, width]);
+
+const y = d3
+  .scaleBand()
+  .domain(d3.range(barChartData.length))
+  .range([height, 0]);
+
+const xAxis = d3.axisTop().scale(x);
+
+const svg = d3
+  .select("#root")
+  .append("svg")
+  .attr("height", height + margin.top + margin.bottom)
+  .attr("width", width + margin.left + margin.right)
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
