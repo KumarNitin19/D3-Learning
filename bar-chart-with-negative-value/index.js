70d10b5daca6e0c47568789e6bff5fac07cee0c5
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-06-21 01:20:39 +0530

    chore: basic files added for bar chart with negative value

---
+++ bar-chart-with-negative-value/index.js
@@ -0,0 +1,13 @@
+console.log("Bar chart with negative value");
+
+const barChartData = [-15, -20, -22, -18, 2, 6, -26, -18];
+
+const margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50,
+};
+
+const width = 960 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
