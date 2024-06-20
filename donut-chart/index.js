814bec63215e14d471dd6872ee396a032482f4c1
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-06-21 00:49:43 +0530

    update: data and height, width and radius added for donut chart

--- donut-chart/index.js
+++ donut-chart/index.js
@@ -1,1 +1,9 @@
 console.log("Donut Chart");
+
+const donutChartData = { a: 9, b: 20, c: 30, d: 8, e: 12 };
+
+const height = 500;
+const width = 960;
+const margin = 40;
+
+const radius = Math.min(height, width) / 2 - margin;
