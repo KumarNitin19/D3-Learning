882d621ebd774b2b55afb1a2e71c313790cb9e82
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-06-28 01:14:44 +0530

    feat: axis created for graph

--- scatter-plot-with-tooltip/index.js
+++ scatter-plot-with-tooltip/index.js
@@ -13,8 +13,8 @@ const margin = {
 const width = 960 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
-const x = d3.scaleLinear([0, width]);
-const y = d3.scaleLinear([height, 0]);
+const x = d3.scaleLinear().range([0, width]);
+const y = d3.scaleLinear().range([height, 0]);
 
 const xAxis = d3.axisBottom().scale(x);
 const yAxis = d3.axisLeft().scale(y);
@@ -28,3 +28,42 @@ const svg = d3
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv(
+  scatterPlotDataUrl,
+  function (d) {
+    return {
+      grLivArea: +d.GrLivArea,
+      salePrice: +d.SalePrice,
+    };
+  },
+  function (data) {
+    x.domain([0, d3.max(data, (d) => d.grLivArea)]);
+    y.domain([0, d3.max(data, (d) => d.salePrice)]);
+
+    svg
+      .append("g")
+      .attr("class", "x axis")
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxis)
+      .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .attr("text-anchor", "end")
+      .attr("fill", "red")
+      .text("GRliveArea");
+
+    svg
+      .append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .attr("fill", "red")
+      .text("Sale Price");
+  }
+);
