5b085127c735e9464787ff728a9a635f4648fb42
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-06-28 00:59:15 +0530

    feat: basic margins and x, y axis added

--- scatter-plot-with-tooltip/index.js
+++ scatter-plot-with-tooltip/index.js
@@ -1,1 +1,20 @@
 console.log("Scatter plot with tooltip");
+
+const scatterPlotDataUrl =
+  "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv";
+
+const margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50,
+};
+
+const width = 960 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+const x = d3.scaleLinear([0, width]);
+const y = d3.scaleLinear([height, 0]);
+
+const xAxis = d3.axisBottom().scale(x);
+const yAxis = d3.axisLeft().scale(y);
