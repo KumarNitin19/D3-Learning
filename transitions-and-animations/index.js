2ab081c6a0f3c69c2d9728252fc3e42c23fcd47b
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-01 01:22:32 +0530

    feat: transiton added to circle

--- transitions-and-animations/index.js
+++ transitions-and-animations/index.js
@@ -21,3 +21,20 @@ rectButton.addEventListener("click", () => {
     .attr("x", 240)
     .attr("y", 140);
 });
+
+d3.select("#circle")
+  .append("svg")
+  .attr("height", 200)
+  .attr("width", 300)
+  .style("border", "1px solid")
+  .append("circle")
+  .attr("class", "circle_shape")
+  .attr("cx", 150)
+  .attr("cy", 100)
+  .attr("r", 50)
+  .attr("fill", "red");
+
+const circleButton = document.getElementById("circle_button");
+circleButton.addEventListener("click", () => {
+  d3.select(".circle_shape").transition().duration(2000).attr("r", 80);
+});
