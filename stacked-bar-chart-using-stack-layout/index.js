c568673e083dabff08eef722395c45c1dd40a2fe
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-02 00:32:42 +0530

    feat: basic files added for stacked bar chart using stack layour

---
+++ stacked-bar-chart-using-stack-layout/index.js
@@ -0,0 +1,11 @@
+console.log("Stacked bar chart using stack layout");
+
+const margin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30,
+};
+
+const width = 960 - margin.right - margin.left;
+const height = 500 - margin.top - margin.bottom;
