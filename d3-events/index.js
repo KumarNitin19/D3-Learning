5c39efe47fc0735b9e2c7d276f3875dc1122a743
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-01 21:41:48 +0530

    feat: rectangles created

--- d3-events/index.js
+++ d3-events/index.js
@@ -1,1 +1,50 @@
 console.log("D3 Events");
+
+const rectData = [
+  {
+    x: "5",
+    y: "5",
+    height: "50",
+    width: "50",
+    color: "#1f77b4",
+  },
+  {
+    x: "5",
+    y: "75",
+    height: "50",
+    width: "50",
+    color: "#ff7f0e",
+  },
+  {
+    x: "5",
+    y: "145",
+    height: "50",
+    width: "50",
+    color: "#2ca02c",
+  },
+  {
+    x: "5",
+    y: "215",
+    height: "50",
+    width: "50",
+    color: "#d62728",
+  },
+];
+
+const svg = d3
+  .select("#root")
+  .append("svg")
+  .attr("height", "100vh")
+  .attr("width", "100%")
+  .style("border", "1px solid")
+  .append("g")
+  .attr("transform", "translate(15,15)");
+svg
+  .selectAll("rect")
+  .data(rectData)
+  .enter()
+  .append("rect")
+  .attr("x", (d) => d.x)
+  .attr("y", (d) => d.y)
+  .attr("width", (d) => d.width)
+  .attr("height", (d) => d.height);
