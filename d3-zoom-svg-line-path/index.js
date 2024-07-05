bc41ff772c29312f5278cf4431df70f02d278024
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-05 23:41:51 +0530

    fix: margins and height, width added

--- d3-zoom-svg-line-path/index.js
+++ d3-zoom-svg-line-path/index.js
@@ -1,1 +1,11 @@
 console.log("d3 zoom svg lines and svg path");
+
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 20,
+  left: 20,
+};
+
+const height = 300 - margin.top - margin.bottom;
+const width = 300 - margin.left - margin.right;
