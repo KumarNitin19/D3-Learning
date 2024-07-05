96f4d3cc05f55c097992a1033ed8cfe5e7e4a03b
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-06 00:13:13 +0530

    feat: zoom added to svg line

--- d3-zoom-svg-line-path/index.js
+++ d3-zoom-svg-line-path/index.js
@@ -52,6 +52,7 @@ const y_axis = svg.append("g").attr("class", "y-axis").call(yAxis);
 
 svg
   .append("g")
+  .attr("id", "line_group")
   .selectAll("line")
   .data([originalLine])
   .enter()
@@ -62,3 +63,24 @@ svg
   .attr("y2", (d) => y(d.y2))
   .attr("stroke-width", (d) => d["stroke-width"])
   .attr("stroke", "red");
+
+function zoomFunction() {
+  const lines = d3.select("#line_group").selectAll("line");
+  const newXAxis = d3.event.transform.rescaleX(x);
+  const newYAxis = d3.event.transform.rescaleY(y);
+
+  x_axis.transition().duration(0).call(xAxis.scale(newXAxis));
+  y_axis.transition().duration(0).call(yAxis.scale(newYAxis));
+
+  const scale = d3.event.transform.k;
+
+  lines
+    .attr("x1", (d) => newXAxis(d.x1))
+    .attr("y1", (d) => newYAxis(d.y1))
+    .attr("x2", (d) => newXAxis(d.x2))
+    .attr("y2", (d) => newYAxis(d.y2))
+    .attr("stroke-width", (d) => scale * d["stroke-width"]);
+}
+
+const zoom = d3.zoom().scaleExtent([0.5, 5]).on("zoom", zoomFunction);
+svg.call(zoom);
