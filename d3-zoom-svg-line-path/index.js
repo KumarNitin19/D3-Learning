dbbbc23ceadd622a0074eb7672c9d214038d8bb7
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-05 23:44:23 +0530

    feat: svg created

--- d3-zoom-svg-line-path/index.js
+++ d3-zoom-svg-line-path/index.js
@@ -7,5 +7,20 @@ const margin = {
   left: 20,
 };
 
-const height = 300 - margin.top - margin.bottom;
-const width = 300 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+const width = 500 - margin.left - margin.right;
+
+const originalLine = {
+  x1: 25,
+  y1: 25,
+  x2: 125,
+  y2: 125,
+  ["stroke-width"]: 2,
+};
+
+const svg = d3
+  .select("#root")
+  .append("svg")
+  .attr("height", height + margin.top + margin.bottom)
+  .attr("width", width + margin.left + margin.right)
+  .style("border", "2px solid");
