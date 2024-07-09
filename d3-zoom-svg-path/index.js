62be1b7a5cc350167753453cd3f2198647efdf24
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-09 23:50:17 +0530

    feat: svg path instruction added

--- d3-zoom-svg-path/index.js
+++ d3-zoom-svg-path/index.js
@@ -102,3 +102,6 @@ const svgPath = d3
   .attr("width", width + margin.left + margin.right)
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+const svgPathInstructions =
+  "M10 315 L 110 215 A 30 50 0 0 1 162.55 162.45 L 172.55 152.45 A 30 50 -45 0 1 215.1 109.9 L 315 10";
