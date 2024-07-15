d0cdbc4ad4dfc8b2c109c12d587b4931da09d2e9
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-15 23:47:16 +0530

    feat: svg element created

--- d3-svg-line/index.js
+++ d3-svg-line/index.js
@@ -1,5 +1,15 @@
 console.log("d3 svg line");
 
+const margin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30,
+};
+
+const width = 500;
+const height = 500;
+
 const lineData = [
   {
     x: 5,
@@ -34,3 +44,12 @@ const linePathGenerator = d3
   .curve(d3.curveBasis);
 
 linePathGenerator(lineData);
+
+const svg = d3
+  .select("#root")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .style("border", "2px solid")
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
