33335c8cd57ca3932f09fa50d7634cf78aa2081f
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-15 23:42:39 +0530

    feat: data adde for line path

--- d3-svg-line/index.js
+++ d3-svg-line/index.js
@@ -1,1 +1,36 @@
 console.log("d3 svg line");
+
+const lineData = [
+  {
+    x: 5,
+    y: 30,
+  },
+  {
+    x: 75,
+    y: 30,
+  },
+  {
+    x: 75,
+    y: 90,
+  },
+  {
+    x: 150,
+    y: 90,
+  },
+  {
+    x: 150,
+    y: 150,
+  },
+  {
+    x: 190,
+    y: 150,
+  },
+];
+
+const linePathGenerator = d3
+  .line()
+  .x((d) => d.x)
+  .y((d) => d.y)
+  .interpolate("linear");
+
+linePathGenerator(lineData);
