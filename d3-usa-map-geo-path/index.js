9caeb88cf2c8bde201cb0feb855131154d08c2c7
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-16 23:50:29 +0530

    feat: geo path added for map

--- d3-usa-map-geo-path/index.js
+++ d3-usa-map-geo-path/index.js
@@ -1,1 +1,8 @@
+import { geoGeometryCollection } from "./usa-data.js";
 console.log("D3 USA map geo path");
+
+const height = 400;
+const width = 400;
+const geo = d3.geoPath();
+
+console.log(geo(geoGeometryCollection));
