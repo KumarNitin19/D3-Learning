dde0053a2b7b86bd3fbf6ebd0db5b9bf7f3316f7
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-16 23:53:05 +0530

    feat: zoom function added for map

--- d3-usa-map-geo-path/index.js
+++ d3-usa-map-geo-path/index.js
@@ -5,4 +5,15 @@ const height = 400;
 const width = 400;
 const geo = d3.geoPath();
 
-console.log(geo(geoGeometryCollection));
+// console.log(geo(geoGeometryCollection));
+
+function zoomFunction() {
+  const zoomVal = d3.event.tranform;
+  const x = zoomVal.x;
+  const y = zoomVal.y;
+  const scale = zoomVal.k;
+  d3.selectAll("path").attr(
+    "transform",
+    `translate(${x},${y}) scale(${scale})`
+  );
+}
