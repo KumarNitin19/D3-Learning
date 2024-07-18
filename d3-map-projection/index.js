f75d5545996db8af827a2c7b946244e089d8d937
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-18 23:53:47 +0530

    feat: svg created

--- d3-map-projection/index.js
+++ d3-map-projection/index.js
@@ -1,1 +1,14 @@
 console.log("D3 map projection");
+
+const geo = d3.geoPath();
+
+const width = 600;
+const height = 600;
+
+const svg = d3
+  .select("#root")
+  .append("svg")
+  .attr("height", height)
+  .attr("width", width)
+  .append("g")
+  .attr("transform", `translate(30, 30)`);
