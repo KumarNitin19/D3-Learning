a1fe0795300aba3f6570c171f1e830901ef8fdb1
anurag-elevano <nitinkumardeshmukh19@gmail.com>
2024-11-14 00:56:06 +0530

    feat: path between function added to draw path

--- path-between/index.js
+++ path-between/index.js
@@ -6,6 +6,32 @@ const d1 = "M0,0c100,0 0,100 100,100c100,0 0,-100 100,-100c100,0 0,100 100,100";
 const width = 928;
 const height = 500;
 
+function pathTween(d1, precision) {
+  return function () {
+    const path0 = this;
+    const path1 = path0.cloneNode();
+    path1.setAttribute("d", d1);
+    const n0 = path0.getTotalLength();
+    const n1 = path1.getTotalLength();
+
+    // Uniform sampling of distance based on specified precision.
+    const distances = [0];
+    const dt = precision / Math.max(n0, n1);
+    let i = 0;
+    while ((i += dt) < 1) distances.push(i);
+    distances.push(1);
+
+    // Compute point-interpolators at each distance.
+    const points = distances.map((t) => {
+      const p0 = path0.getPointAtLength(t * n0);
+      const p1 = path1.getPointAtLength(t * n1);
+      return d3.interpolate([p0.x, p0.y], [p1.x, p1.y]);
+    });
+
+    return (t) => (t < 1 ? "M" + points.map((p) => p(t)).join("L") : d1);
+  };
+}
+
 const svg = d3
   .select("#root")
   .append("svg")
