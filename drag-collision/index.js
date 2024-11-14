f9eb25c417a5b725774434320a2d1b16c333a5be
anurag-elevano <nitinkumardeshmukh19@gmail.com>
2024-11-15 02:02:26 +0530

    feat: circles added

--- drag-collision/index.js
+++ drag-collision/index.js
@@ -11,3 +11,35 @@ const canvas = d3
   .attr("width", width)
   .attr("style", "border:2px solid #000;");
 const context = document?.getElementById("myCanvas")?.getContext("2d");
+
+const circles = d3.range(324).map((i) => ({
+  x: (i % 25) * (radius + 1) * 2,
+  y: Math.floor(i / 25) * (radius + 1) * 2,
+}));
+
+const simulation = d3
+  .forceSimulation(circles)
+  .force("collide", d3.forceCollide(radius + 1).iterations(4))
+  .on("tick", drawCircles);
+
+d3.select("#myCanvas").call(d3.drag().subject(subject));
+
+function drawCircles() {
+  context.clearRect(0, 0, width, height);
+  context.save();
+  circles.forEach(drawCircle);
+  context.strokeStyle = "#fff";
+  context.stroke();
+}
+
+function drawCircle(d) {
+  context.beginPath();
+  context.fillStyle = d.type;
+  context.moveTo(d.x + radius, d.y);
+  context.arc(d.x, d.y, radius, 0, 2 * Math.PI);
+  context.fill();
+}
+
+function subject(event) {
+  return simulation.find(event.x, event.y, radius);
+}
